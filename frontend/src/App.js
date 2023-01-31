a1b978dc0813d2ae785e7a0ab3d3c00f3ce004eb
Thuy Nguyen <thuynm@visionnetwork.vn>
2023-01-31 17:51:27 +0700

    Update notesApiSlice & usersApiSlice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Public from './components/Public'
-import Login from './features/auth/Login';
-import DashLayout from './components/DashLayout'
-import Welcome from './features/auth/Welcome'
-import NotesList from './features/notes/NotesList'
-import UsersList from './features/users/UsersList'
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
+import EditUser from "./features/users/EditUserForm";
+import EditNote from "./features/notes/EditNote";
+import NewUserForm from "./features/users/NewUserForm";
+import NewNote from "./features/notes/NewNote";
 
 function App() {
   return (
@@ -15,19 +19,21 @@ function App() {
         <Route path="login" element={<Login />} />
 
         <Route path="dash" element={<DashLayout />}>
-
           <Route index element={<Welcome />} />
 
           <Route path="notes">
             <Route index element={<NotesList />} />
+            <Route path=":id" element={<EditUser />} />
+            <Route path="new" element={<NewNote />} />
           </Route>
 
           <Route path="users">
             <Route index element={<UsersList />} />
+            <Route path=":id" element={<EditUser />} />
+            <Route path="new" element={<NewUserForm />} />
           </Route>
-
-        </Route>{/* End Dash */}
-
+        </Route>
+        {/* End Dash */}
       </Route>
     </Routes>
   );
--- frontend/src/features/notes/notesApiSlice.js
+++ frontend/src/features/notes/notesApiSlice.js
@@ -1,6 +1,5 @@
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
 import { apiSlice } from "../../app/api/apiSlice";
-
-const { createEntityAdapter, createSelector } = require("@reduxjs/toolkit");
 
 const notesAdapter = createEntityAdapter({
   sortComparer: (a, b) =>
@@ -16,7 +15,6 @@ export const notesApiSlice = apiSlice.injectEndpoints({
       validateStatus: (response, result) => {
         return response.status === 200 && !result.isError;
       },
-      keepUnusedDataFor: 5,
       transformResponse: (responseData) => {
         const loadedNotes = responseData.map((note) => {
           note.id = note._id;
@@ -28,32 +26,64 @@ export const notesApiSlice = apiSlice.injectEndpoints({
         if (result?.ids) {
           return [
             { type: "Note", id: "LIST" },
-            ...result.ids.map((id) => ({ type: "note", id })),
+            ...result.ids.map((id) => ({ type: "Note", id })),
           ];
         } else return [{ type: "Note", id: "LIST" }];
       },
     }),
+    addNewNote: builder.mutation({
+      query: (initialNote) => ({
+        url: "/notes",
+        method: "POST",
+        body: {
+          ...initialNote,
+        },
+      }),
+      invalidatesTags: [{ type: "Note", id: "LIST" }],
+    }),
+    updateNote: builder.mutation({
+      query: (initialNote) => ({
+        url: "/notes",
+        method: "PATCH",
+        body: {
+          ...initialNote,
+        },
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: "Note", id: arg.id }],
+    }),
+    deleteNote: builder.mutation({
+      query: ({ id }) => ({
+        url: `/notes`,
+        method: "DELETE",
+        body: { id },
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: "Note", id: arg.id }],
+    }),
   }),
 });
 
-export const { useGetNotesQuery } = notesApiSlice;
+export const {
+  useGetNotesQuery,
+  useAddNewNoteMutation,
+  useUpdateNoteMutation,
+  useDeleteNoteMutation,
+} = notesApiSlice;
 
-// Returns the query result object
+// returns the query result object
 export const selectNotesResult = notesApiSlice.endpoints.getNotes.select();
 
-// Creates memozied selector
+// creates memoized selector
 const selectNotesData = createSelector(
   selectNotesResult,
-  (notesResult) => notesResult.data // normalized state object with ids and entities
+  (notesResult) => notesResult.data // normalized state object with ids & entities
 );
 
-// getSelectors creates these selector and we rename them with aliases using destructuring
+//getSelectors creates these selectors and we rename them with aliases using destructuring
 export const {
   selectAll: selectAllNotes,
   selectById: selectNoteById,
   selectIds: selectNoteIds,
-
-  //   Pass in a selector that returns the notes slice of state
+  // Pass in a selector that returns the notes slice of state
 } = notesAdapter.getSelectors(
   (state) => selectNotesData(state) ?? initialState
 );
