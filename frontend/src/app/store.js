f6157cc110621da0c0d5978072d9cef8fd49c7df
Thuy Nguyen <thuynm@visionnetwork.vn>
2023-02-02 11:58:48 +0700

    Compleled app

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -10,7 +10,7 @@ export const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
-  devTools: true,
+  devTools: !process.env.NODE_ENV !== "production",
 });
 
 setupListeners(store.dispatch);
